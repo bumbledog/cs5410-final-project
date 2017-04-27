ffc0168a3a356b8947f11221200fe4edcce92171
jensenjesseh <jensen.jesse.h@gmail.com>
2017-04-26 23:10:40 -0600

    removed unnecessary files

--- public/scripts/audio.js
+++ public/scripts/audio.js
@@ -29,7 +29,6 @@ function loadAudio(){
     that.sounds['assets/grunt'] = loadSound('assets/grunt.wav');
     that.sounds['assets/main-menu-music'] = loadSound('assets/main-menu-music.mp3');
     that.sounds['assets/song-1'] = loadSound('assets/song-1.mp3');
-    that.sounds['assets/sword-hit'] = loadSound('assets/sword-hit.wav');
     that.sounds['assets/slime-sound'] = loadSound('assets/slime-sound.wav');
     that.sounds['assets/bat-sound'] = loadSound('assets/bat-sound3.wav');
     that.sounds['assets/coin-sound'] = loadSound('assets/coin-sound.wav');
@@ -37,11 +36,9 @@ function loadAudio(){
     that.sounds['assets/main-menu-music'].volume = 0.05;
     that.sounds['assets/song-1'].volume = 0.05;
     that.sounds['assets/sword-swipe'].volume = 0.05;
-    that.sounds['assets/sword-hit'].volume = 0.05;
     that.sounds['assets/slime-sound'].volume = 0.50;
     that.sounds['assets/bat-sound'].volume = 0.07;
     that.sounds['assets/coin-sound'].volume = 0.50;
-    //that.sounds['assets/coin-sound'].volume = 0.10;
 };
 
 loadAudio();
